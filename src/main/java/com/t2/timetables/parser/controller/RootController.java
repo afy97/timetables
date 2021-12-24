499a6f05adb059158aaf26ad93dc6f0943d43d06
furkan.yildirim <Gazoz123>
2021-12-24 19:22:50 +0300

    poi init

--- src/main/java/com/t2/timetables/parser/controller/RootController.java
+++ src/main/java/com/t2/timetables/parser/controller/RootController.java
@@ -1,20 +1,40 @@
 package com.t2.timetables.parser.controller;
 
+import com.t2.timetables.parser.service.ParsingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.view.RedirectView;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 @RestController
 public class RootController {
+    @Autowired
+    ParsingService parsingService;
+
     @GetMapping("/")
     public RedirectView redirect() {
         return new RedirectView("/index.html");
     }
 
     @PostMapping("/")
-    public ResponseEntity<MultipartFile> parse(@RequestParam MultipartFile input) {
-        return ResponseEntity.status(HttpStatus.OK).body(input);
+    public ResponseEntity<Resource> parse(@RequestParam MultipartFile input) throws IOException {
+        parsingService.parse(input.getInputStream());
+        return ResponseEntity
+                .ok()
+                .contentLength(input.getSize())
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(new InputStreamResource(input.getInputStream()));
     }
 }
--- src/main/java/com/t2/timetables/parser/service/ParsingService.java
+++ src/main/java/com/t2/timetables/parser/service/ParsingService.java
@@ -1,8 +1,33 @@
 package com.t2.timetables.parser.service;
 
+import org.apache.poi.ss.usermodel.*;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 @Service
 public class ParsingService {
+    public void parse(InputStream stream) throws IOException {
+        Workbook workbook = WorkbookFactory.create(stream);
+        Sheet sheet = workbook.getSheetAt(0);
+        DataFormatter formatter = new DataFormatter();
 
+        bailout:
+        for (Row row : sheet) {
+            for (Cell cell : row) {
+                String cellValue = formatter.formatCellValue(cell);
+
+                if (StringUtils.hasText(cellValue)) {
+                    System.out.print(cellValue + "\t");
+                } else {
+                    break bailout;
+                }
+            }
+            System.out.println();
+        }
+
+        stream.close();
+    }
 }
